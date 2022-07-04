f8c11759c24e9c7732ac07feddeb80c071597a02
Florencia Parraguez <florenciaparraguezsepulveda@gmail.com>
2022-07-04 11:06:26 -0400

    agregué el contexto de datos, en el home controller se creo uan nueva instancia y que sea preentaran lso datos en una lista

--- Tarea1/Tarea1/Controllers/HomeController.cs
+++ Tarea1/Tarea1/Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tarea1.Models;
 
 namespace Tarea1.Controllers
 {
     public class HomeController : Controller
     {
+        private Models.tarea_1Entities1 contextoDatos;
+
+        HomeController()
+        {
+            contextoDatos = new tarea_1Entities1();
+        }
         public ActionResult Index()
         {
-            return View();
+            var productos = contextoDatos.producto.ToList();
+            return View(productos);
         }
 
         public ActionResult About()
